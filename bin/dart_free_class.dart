794f3bdcbad6017aac6a456556328fb8705876f2
hoomanjb <hoomanjavan74@gmail.com>
2023-11-03 13:02:29 +0400

    const and final

--- bin/dart_free_class.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_free_class/dart_free_class.dart' as dart_free_class;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_free_class.calculate()}!');
-}
---
+++ bin/variables.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  const name = "chert";
+  print(name);
+
+  final ages = [12, 21, 35, 44];
+  print(ages);
+  ages.remove(1);
+  print(ages);
+
+}
