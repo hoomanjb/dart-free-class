b9f1300c222d355d43cd4daae6197c674e5d3992
hoomanjb <hoomanjavan74@gmail.com>
2023-11-03 14:57:02 +0400

    data type done

--- bin/variables.dart
+++ bin/variables.dart
@@ -1,18 +1,24 @@
 void main(List<String> args) {
-  const name = "chert";
-  print(name);
+  // const name = "chert";
+  // print(name);
+  //
+  // final ages = [12, 21, 35, 44];
+  // print(ages);
+  // ages.remove(1);
+  // print(ages);
+  //
+  // var address = '123 main Street';
+  // print(address);
+  // address = '444 main Street';
+  // print(address);
+  // print(address.length);
+  // address = address.replaceAll('main', 'Bla');
+  // print(address);
 
-  final ages = [12, 21, 35, 44];
-  print(ages);
-  ages.remove(1);
-  print(ages);
+  String name = 'foo';
+  var address ;
+  name =  '';
+  address = 2;
 
-  var address = '123 main Street';
-  print(address);
-  address = '444 main Street';
-  print(address);
-  print(address.length);
-  address = address.replaceAll('main', 'Bla');
-  print(address);
 
 }
